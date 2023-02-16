b2bdbb3fd4d01a32b075d70658f3f1cbd670dd7a
JackMor9 <124622958+JackMor9@users.noreply.github.com>
2023-02-16 09:45:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from classe_livre import *
 
+from classe_livre import *
+list_livres = []
 livre_1 = Livre()
-livre_1.Titre = "Chercher Sam"
-print(livre_1.Titre)
+livre_1.Titre = input("Entrez le titre du livre : " )
+livre_1.Auteur = input("Entrez l'auteur du livre : ")
+while True :
+    try:
+        livre_1.Prix = float(input("Entrez le prix du livre : "))
+    except :
+        print("Entrez un nombre entier svp.. : ")
+
+    else :
+        if livre_1.Prix < 50 :
+            break
+        else :
+            print("Entrer un nombre inferieur à 50")
+print(livre_1.Titre , livre_1.Auteur, livre_1.Prix)
+
+list_livres.append(livre_1)
+
+
 
